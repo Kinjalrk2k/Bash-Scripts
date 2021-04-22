195c04085e98b2d728c44fcddbd9f9b3bd01728c
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-04-22 10:11:30 +0530

    Update config.sh

--- RDP/config.sh
+++ RDP/config.sh
@@ -3,3 +3,13 @@ sudo sed -i 's/3389/3390/g' /etc/xrdp/xrdp.ini
 sudo sed -i 's/max_bpp=32/#max_bpp=32\nmax_bpp=128/g' /etc/xrdp/xrdp.ini
 sudo sed -i 's/xserverbpp=24/#xserverbpp=24\nxserverbpp=128/g' /etc/xrdp/xrdp.ini
 echo xfce4-session > ~/.xsession
+
+
+sudo nano /etc/xrdp/startwm.sh
+!comment these lines to:
+#test -x /etc/X11/Xsession && exec /etc/X11/Xsession
+#exec /bin/sh /etc/X11/Xsession
+
+!add these lines:
+# xfce
+startxfce4
